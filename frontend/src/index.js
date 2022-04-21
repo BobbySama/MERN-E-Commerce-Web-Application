123ad18f6499aca1864cbc769096e8e65c437cfb
BobbySama <bobbyzarcu@gmail.com>
2022-04-21 14:48:42 +0300

    started the backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './bootstrap.min.css';
+import './css/index.css';
+import './css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
