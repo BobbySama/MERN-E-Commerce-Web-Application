b75538c7ff82080d02095b141ed422dfa4a8fc7f
BobbySama <bobbyzarcu@gmail.com>
2022-04-28 19:00:00 +0300

    created reducers and action reducers (redux)

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,31 +1,35 @@
-// eslint-disable-next-line
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  // eslint-disable-next-line
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((productElem) => (
-          <Col key={productElem._id} xs={6} sm={6} md={6} lg={3}>
-            <Product productProp={productElem} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading!!!!!!!!!!!!!!!</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((productElem) => (
+            <Col key={productElem._id} xs={6} sm={6} md={6} lg={3}>
+              <Product productProp={productElem} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 };
